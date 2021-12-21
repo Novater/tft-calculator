51ecde2d93b96e1e954dcf3cd6ca8f891991550e
Novater <cookiesnmilkjk@gmail.com>
2021-12-20 23:38:56 -0500

    working on layout

--- src/components/ProbabilityChart.jsx
+++ src/components/ProbabilityChart.jsx
@@ -6,7 +6,7 @@ import {
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import faker from 'faker';
@@ -15,15 +15,16 @@ ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
 const options = {
   responsive: true,
+  maintainAspectRatio: true,
   plugins: {
     legend: {
-      position: 'top'
+      position: 'top',
     },
     title: {
       display: true,
-      text: 'Chart.js Bar Chart'
-    }
-  }
+      text: 'Chart.js Bar Chart',
+    },
+  },
 };
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
@@ -34,14 +35,14 @@ const data = {
     {
       label: 'Dataset 1',
       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(255, 99, 132, 0.5)'
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
     {
       label: 'Dataset 2',
       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(53, 162, 235, 0.5)'
-    }
-  ]
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
 };
 
 export default function ProbabilityChart() {
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,11 +1,11 @@
-import ProbabilityChart from "../components/ProbabilityChart";
+import ProbabilityChart from '../components/ProbabilityChart';
 
 export default function Home() {
   return (
     <div className="homepage-container flex">
       <section className="banner-container font-sans font-semibold" />
       <section className="calculator-container">
-        <div className="input-container">INPUT</div>
+        <div className="input-container">Form</div>
         <div className="graph-container"><ProbabilityChart /></div>
       </section>
       <section className="footer-container">FOOTER</section>
