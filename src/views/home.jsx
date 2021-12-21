f2535d9b2076ddd3f3281dfc6dcb4e03dc742161
Novater <cookiesnmilkjk@gmail.com>
2021-12-20 23:47:02 -0500

    some more updates

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -5,10 +5,16 @@ export default function Home() {
     <div className="homepage-container flex">
       <section className="banner-container font-sans font-semibold" />
       <section className="calculator-container">
-        <div className="input-container">Form</div>
+        <div className="input-container">
+          <h2>Champion Tier:</h2>
+          <h2>Level:</h2>
+          <h2>Gold To Roll:</h2>
+        </div>
         <div className="graph-container"><ProbabilityChart /></div>
       </section>
-      <section className="footer-container">FOOTER</section>
+      <section className="footer-container">
+        <h1 className="font-sans">Tool by Novater.io</h1>
+      </section>
     </div>
   );
 }
