bc7685a1222169fae819bebb4c66ad5380a5f654
Novater <cookiesnmilkjk@gmail.com>
2022-01-01 01:46:06 -0500

    working on functionality

---
+++ src/class/gamepool.js
@@ -0,0 +1,150 @@
+/* eslint no-underscore-dangle: "off", implicit-arrow-linebreak: "off", operator-linebreak: "off", max-len: "off" */
+
+import _ from 'lodash';
+
+export default class GamePool {
+  constructor() {
+    this.levelTierMatrix = {};
+    this.champsInTier = {};
+    this.uniqueChampsInTier = {};
+    this.tiers = [];
+    this.levels = [];
+  }
+
+  _F = (num) => {
+    if (num === 0) return 1;
+    if (num === 1) return 1;
+
+    return num * this._F(num - 1);
+  };
+
+  _C = (total, selected) => this._F(total) / (this._F(total - selected) * this._F(selected));
+
+  populateLevels = (levels) => {
+    this.levels = levels;
+    _.map(this.levels, (level) => {
+      this.levelTierMatrix[level.getId()] = {};
+    });
+  };
+
+  populateTiers = (tiers) => {
+    this.tiers = tiers;
+    _.map(this.levels || [], (level) => {
+      _.map(this.tiers, (tier) => {
+        this.levelTierMatrix[level.getId()][tier.getId()] = 0;
+      });
+    });
+  };
+
+  setTierProbabilityForLevel = ({ levelId, tierProbabilities }) => {
+    if (!levelId || !tierProbabilities) return;
+
+    if (!this.levelTierMatrix[levelId]) return;
+
+    _.map(_.keys(tierProbabilities), (tier) => {
+      this.levelTierMatrix[levelId][tier] = tierProbabilities[tier];
+    });
+  };
+
+  setNChampsInTier = ({ tierId, number }) => {
+    this.champsInTier[tierId] = number;
+  };
+
+  setNUniqueChampsInTier = ({ tierId, number }) => {
+    this.uniqueChampsInTier[tierId] = number;
+  };
+
+  setChancesPerRoll = (number) => {
+    this.chancesPerRoll = number;
+  };
+
+  setGoldPerRoll = (number) => {
+    this.goldPerRoll = number;
+  };
+
+  getGoldPerRoll = () => this.goldPerRoll;
+
+  getLevels = () => this.levels;
+
+  getTiers = () => this.tiers;
+
+  getRollEV = ({
+    levelId,
+    tierId,
+    gold,
+    championsOwned,
+    otherChampionsOwned,
+  }) => {
+    if (!levelId || !tierId || !gold) return {};
+
+    const numberRolls = gold / this.goldPerRoll;
+    const probPerChance =
+      this.levelTierMatrix[levelId][tierId] *
+      ((this.getNChampsInTier(tierId) - championsOwned) /
+        (this.getNChampsInTier(tierId) * this.getNUniqueChampsInTier(tierId) - championsOwned - otherChampionsOwned));
+
+    return {
+      ev: numberRolls * this.chancesPerRoll * probPerChance,
+      numRolls: numberRolls,
+      probPerChance,
+    };
+  };
+
+  getRollProbability = ({
+    levelId,
+    tierId,
+    gold,
+    championsOwned,
+    otherChampionsOwned,
+  }) => {
+    if (!levelId || !tierId || !gold) return {};
+
+    const numberRolls = gold / this.goldPerRoll;
+    const rollCorrectTierPr = this.levelTierMatrix[levelId][tierId];
+
+    let probabilityRoll0 = 0;
+    for (let i = 0; i <= this.chancesPerRoll; i += 1) {
+      probabilityRoll0 +=
+        this._C(this.chancesPerRoll, i) *
+        (rollCorrectTierPr * (1 - this._getSingleRollProbability(tierId, championsOwned, otherChampionsOwned))) ** i *
+        (1 - rollCorrectTierPr) ** (this.chancesPerRoll - i);
+    }
+
+    // let finalPr = 1;
+    // for (let i = 2; i <= copies; i += 1) {
+    //   let probabilityRollX = 0;
+    //   for (let j = 0; j <= this.chancesPerRoll - i; i += 1) {
+    //     probabilityRollX +=
+    //       this._C(this.chancesPerRoll, copies) *
+    //       (rollCorrectTierPr * this._getSingleRollProbability(tierId)) ** i *
+    //       ((1 - rollCorrectTierPr) ** j *
+    //         (rollCorrectTierPr * (1 - this._getSingleRollProbability(tierId)) ** (this.chancesPer))) **
+    //         (this.chancesPerRoll - copies);
+    //   }
+    // }
+
+    // console.log('[Probability]', totalProbability);
+
+    return {
+      probability: 1 - probabilityRoll0 ** numberRolls,
+    };
+  };
+
+  _getSingleRollProbability = (tierId, championsOwned, otherChampionsOwned) => {
+    const nThis = this.getNChampsInTier(tierId);
+    const nUniqueChars = this.getNUniqueChampsInTier(tierId);
+
+    return (nThis - championsOwned) / (nThis * nUniqueChars - championsOwned - otherChampionsOwned);
+  };
+
+  getProbabilityMatrix = () => this.levelTierMatrix || {};
+
+  getNChampsInTier = (tierId) => (tierId ? this.champsInTier[tierId] : this.champsInTier);
+
+  getNUniqueChampsInTier = (tierId) => {
+    if (tierId) {
+      return this.uniqueChampsInTier[tierId];
+    }
+    return this.uniqueChampsInTier;
+  };
+}
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,20 +1,27 @@
+/* eslint no-debugger: 1 */
+
 import _ from 'lodash';
 import React, { useState } from 'react';
 import { MathJax, MathJaxContext } from 'better-react-mathjax';
-import GamePool from '../config/gamepool';
-import Level from '../config/level';
-import Tier from '../config/tier';
+import GamePool from '../class/gamepool';
+import Level from '../class/level';
+import Tier from '../class/tier';
 import Icon from '../components/Icon';
 
 export default function Home() {
   const [goldToRoll, setGoldToRoll] = useState(0);
+  const [prelimGoldToRoll, setPrelimGoldToRoll] = useState(0);
   const [level, setLevel] = useState(1);
   const [tier, setTier] = useState(1);
-  const [expToLevel, setExpToLevel] = useState(0);
+  const [championsOwned, setChampionsOwned] = useState(0);
+  const [prelimChampionsOwned, setPrelimChampionsOwned] = useState(0);
+  const [otherChampionsOwned, setOtherChampionsOwned] = useState(0);
+  const [prelimOtherChampionsOwned, setPrelimOtherChampionsOwned] = useState(0);
 
   const gamePool = new GamePool();
   const levels = [];
   const tiers = [];
+
   for (let i = 1; i <= 11; i += 1) {
     const newLevel = new Level(i, i);
     levels.push(newLevel);
@@ -23,6 +30,7 @@ export default function Home() {
     const newTier = new Tier(`${i}*`, i);
     tiers.push(newTier);
   }
+
   gamePool.populateLevels(levels);
   gamePool.populateTiers(tiers);
   gamePool.setTierProbabilityForLevel({
@@ -163,30 +171,64 @@ export default function Home() {
   gamePool.setChancesPerRoll(5);
   gamePool.setGoldPerRoll(2);
 
-  console.log(gamePool.getProbabilityMatrix());
-
   function setNewLevel(event) {
     let newVal = parseInt(event.target.value, 10);
     newVal = Number.isNaN(newVal) ? 0 : newVal;
     setLevel(newVal);
+    setPrelimOtherChampionsOwned(0);
+    setOtherChampionsOwned(0);
+    setPrelimChampionsOwned(0);
+    setChampionsOwned(0);
   }
 
   function setNewTier(event) {
     let newVal = parseInt(event.target.value, 10);
     newVal = Number.isNaN(newVal) ? 0 : newVal;
     setTier(newVal);
+    setPrelimOtherChampionsOwned(0);
+    setOtherChampionsOwned(0);
+    setPrelimChampionsOwned(0);
+    setChampionsOwned(0);
+  }
+
+  function setNewPrelimGoldToRoll(event) {
+    let newVal = parseInt(event.target.value, 10);
+    newVal = Number.isNaN(newVal) ? 0 : newVal;
+    setPrelimGoldToRoll(newVal);
   }
 
   function setNewGoldToRoll(event) {
     let newVal = parseInt(event.target.value, 10);
     newVal = Number.isNaN(newVal) ? 0 : newVal;
-    setGoldToRoll(newVal);
+    setPrelimGoldToRoll(Math.ceil(newVal / 2) * 2);
+    setGoldToRoll(Math.ceil(newVal / 2) * 2);
   }
 
-  function setNewExpToLevel(event) {
+  function setNewChampionsOwned(event) {
+    setPrelimChampionsOwned(event.target.value);
+    setChampionsOwned(event.target.value);
+  }
+
+  function setNewOtherChampionsOwned(event) {
+    setPrelimOtherChampionsOwned(event.target.value);
+    setOtherChampionsOwned(event.target.value);
+  }
+
+  function setNewPrelimChampionsOwned(event) {
     let newVal = parseInt(event.target.value, 10);
     newVal = Number.isNaN(newVal) ? 0 : newVal;
-    setExpToLevel(newVal);
+    newVal = newVal > gamePool.getNChampsInTier(tier)
+      ? gamePool.getNChampsInTier(tier) : newVal;
+    setPrelimChampionsOwned(newVal);
+  }
+
+  function setNewPrelimOtherChampionsOwned(event) {
+    let newVal = parseInt(event.target.value, 10);
+    newVal = Number.isNaN(newVal) ? 0 : newVal;
+    const maxVal = gamePool.getNChampsInTier(tier)
+      * gamePool.getNUniqueChampsInTier(tier) - gamePool.getNChampsInTier(tier);
+    newVal = newVal > maxVal ? maxVal : newVal;
+    setPrelimOtherChampionsOwned(newVal);
   }
 
   return (
@@ -195,47 +237,55 @@ export default function Home() {
       <section className="calculator-container">
         <div className="input-container">
           <div className="input-group">
-            <label htmlFor="level" className="font-sans font-semibold">
-              CURRENT LEVEL
-              <div className="btn-group">
-                {
-                  _.map(gamePool.getLevels(), (thisLevel) => (
-                    <button type="button" className={`level-btn${thisLevel.getId().toString() === level.toString() ? ' selected' : ''}`} onClick={setNewLevel} value={thisLevel.getId()}>
-                      {thisLevel.getName()}
-                    </button>
-                  ))
-                }
-              </div>
-            </label>
-
+            <div className="input-row">
+              <label htmlFor="level" className="font-sans font-semibold" id="curr-level">
+                Current Level
+                <div className="btn-group">
+                  {
+                    _.map(gamePool.getLevels(), (thisLevel, idx) => (
+                      <button type="button" key={`level-${idx}`} className={`level-btn${thisLevel.getId().toString() === level.toString() ? ' selected' : ''}`} onClick={setNewLevel} value={thisLevel.getId()}>
+                        {thisLevel.getName()}
+                      </button>
+                    ))
+                  }
+                </div>
+              </label>
+            </div>
           </div>
-          <div className="input-group">
-            <label htmlFor="tier" className="font-sans font-semibold">
-              <p>CHAMPION TIER</p>
-              <div className="btn-group">
-                {
-                  _.map(gamePool.getTiers(), (thisTier) => (
-                    <button type="button" id={`tier-${thisTier.getId().toString()}`} className={`level-btn${thisTier.getId().toString() === tier.toString() ? ' selected' : ''}`} onClick={setNewTier} value={thisTier.getId()}>
-                      {thisTier.getName()}
-                    </button>
-                  ))
-                }
-              </div>
-            </label>
+          <div className="input-group" id="champion-tier">
+            <div className="input-row">
+              <label htmlFor="tier" className="font-sans font-semibold">
+                <p>Champion Tier</p>
+                <div className="btn-group">
+                  {
+                    _.map(gamePool.getTiers(), (thisTier, idx) => (
+                      <button type="button" key={`tier-${idx}`} id={`tier-${thisTier.getId().toString()}`} className={`level-btn${thisTier.getId().toString() === tier.toString() ? ' selected' : ''}`} onClick={setNewTier} value={thisTier.getId()}>
+                        {thisTier.getName()}
+                      </button>
+                    ))
+                  }
+                </div>
+              </label>
+            </div>
+            <div className="input-row">
+              <label htmlFor="champions-owned" className="font-sans font-semibold">
+                <p>Copies Gone</p>
+                <input id="champions-owned" value={prelimChampionsOwned} type="number" min="0" onChange={setNewPrelimChampionsOwned} data-tier={tier} onBlur={setNewChampionsOwned} />
+              </label>
+            </div>
+            <div className="input-row">
+              <label htmlFor="other-champions-owned" className="font-sans font-semibold">
+                <p>Same Tier Copies Gone</p>
+                <input id="other-champions-owned" value={prelimOtherChampionsOwned} type="number" min="0" onChange={setNewPrelimOtherChampionsOwned} data-tier={tier} onBlur={setNewOtherChampionsOwned} />
+              </label>
+            </div>
           </div>
-          <div className="input-group">
+          <div className="input-group" id="gold-and-exp">
             <label htmlFor="goldToRoll" className="font-sans font-semibold">
-              <p style={{ display: 'none' }}>GOLD TO ROLL</p>
               <div className="input-row">
-                <input id="goldToRoll" type="text" value={goldToRoll} onChange={setNewGoldToRoll} />
+                <p style={{ display: 'none' }}>GOLD TO ROLL</p>
+                <input id="goldToRoll" type="number" value={prelimGoldToRoll} onChange={setNewPrelimGoldToRoll} onBlur={setNewGoldToRoll} min="0" step="2" />
                 <Icon type="coins" />
-              </div>
-            </label>
-            <label htmlFor="expToNextLevel" className="font-sans font-semibold">
-              <p style={{ display: 'none' }}>EXP TO NEXT LEVEL</p>
-              <div className="input-row">
-                <input id="goldToLevel" type="text" value={expToLevel} onChange={setNewExpToLevel} />
-                <Icon type="levelup" />
               </div>
             </label>
           </div>
@@ -245,45 +295,40 @@ export default function Home() {
             <div>
               <MathJaxContext>
                 <MathJax dynamic>
-                  {`\\(E\\) (Specific ${tier} Cost at Level ${level} rolling ${goldToRoll} gold)`}
+                  {`\\(E\\) (Desired ${tier} Cost at Level ${level} rolling ${goldToRoll} gold)`}
                 </MathJax>
                 <MathJax dynamic>
-                  {`\\(=\\) #Rolls for ${goldToRoll} Gold * #Slots per Roll * P(${tier} Cost/Roll at Level ${level}) * #Copies of Specific ${tier} Cost / #Total Copies of ${tier} Costs`}
+                  {`\\(\\leq \\frac{${goldToRoll || 0}}{${gamePool.getGoldPerRoll()}} * ${!_.isEmpty(gamePool.getProbabilityMatrix()) ? gamePool.getProbabilityMatrix()[level][tier] : 0} * \\frac{${gamePool.getNChampsInTier(tier)}}{${gamePool.getNChampsInTier(tier)} * ${gamePool.getNUniqueChampsInTier(tier)}}\\)`}
                 </MathJax>
                 <MathJax dynamic>
-                  {`\\(\\leq \\frac{${goldToRoll || 0}}{${gamePool.getGoldPerRoll()}} * ${gamePool.getProbabilityMatrix()[level][tier]} * \\frac{${gamePool.getNChampsInTier(tier)}}{${gamePool.getNChampsInTier(tier)} * ${gamePool.getNUniqueChampsInTier(tier)}}\\)`}
+                  {'\\(\\approx \\hspace{1mm} \\)'}
+                  {
+                    gamePool.getRollEV({
+                      levelId: level,
+                      tierId: tier,
+                      gold: goldToRoll || 0,
+                      championsOwned,
+                      otherChampionsOwned,
+                    }).ev || 0
+                  }
+                </MathJax>
+                <MathJax dynamic>
+                  {'\\(P\\) (Rolling at least one)'}
+                </MathJax>
+                <MathJax dynamic>
+                  {'\\(\\approx \\hspace{1mm} \\)'}
+                  {
+                    gamePool.getRollProbability({
+                      levelId: level,
+                      tierId: tier,
+                      gold: goldToRoll || 0,
+                      copies: 1,
+                      championsOwned,
+                      otherChampionsOwned,
+                    }).probability || 0
+                  }
                 </MathJax>
               </MathJaxContext>
-              {
-                gamePool.getRollEV({
-                  levelId: level,
-                  tierId: tier,
-                  gold: goldToRoll || 0,
-                }).ev || 0
-              }
-            </div>
-          </div>
-          <div className="printed-stats">
-            <div>
-              <MathJaxContext>
-                <MathJax dynamic>
-                  {`\\(E\\) (Specific ${tier} Cost at Level ${level + 1} rolling ${goldToRoll - expToLevel} gold)`}
-                </MathJax>
-                <MathJax dynamic>
-                  {`\\(=\\) #Rolls for ${goldToRoll - expToLevel} Gold * #Slots per Roll * P(${tier} Cost/Roll at Level ${level + 1}) * #Copies of Specific ${tier} Cost / #Total Copies of ${tier} Costs`}
-                </MathJax>
-                <MathJax dynamic>
-                  {`\\(\\leq \\frac{${goldToRoll - expToLevel * 4 || 0}}{${gamePool.getGoldPerRoll()}} * ${gamePool.getProbabilityMatrix()[level + 1][tier]} * \\frac{${gamePool.getNChampsInTier(tier)}}{${gamePool.getNChampsInTier(tier)} * ${gamePool.getNUniqueChampsInTier(tier)}}\\)`}
-                </MathJax>
-              </MathJaxContext>
-              {'= '}
-              {
-                gamePool.getRollEV({
-                  levelId: level + 1,
-                  tierId: tier,
-                  gold: goldToRoll - expToLevel || 0,
-                }).ev || 0
-              }
             </div>
           </div>
         </div>
